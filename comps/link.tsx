649e550e4e44661700ae2008cf3c8d8e869ae8d5
Saurya Gur <saurya.gur@outlook.com>
2025-01-04 23:56:37 +0530

    Navbar theme

--- comps/link.tsx
+++ comps/link.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 interface LinkProps {
-  href: string;
-  children: React.ReactNode;
-  className?: string;
-  onClick?: () => void;
+    href: string;
+    children: React.ReactNode;
+    className?: string;
+    onClick?: () => void;
 }
 
-export default function Link({ href, children, className, onClick }: LinkProps) {
-  return (
-    <a href={href} className={className} onClick={onClick}>
-      {children}
-    </a>
-  );
+export default function Link({href, children, className, onClick}: LinkProps) {
+    return (
+        <a href={href} className={className} onClick={onClick}>
+            {children}
+        </a>
+    );
 }
